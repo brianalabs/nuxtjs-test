960f449d27eea7d3362344f5f3b5d6c53bbb3def
Peter Kim <peterkimzz69@gmail.com>
2022-12-21 22:14:06 +0900

    add onAuthStatechange

--- app.vue
+++ app.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { supabase } from "./utils/supabase.utils";
+
 useHead({
   meta: [
     {
@@ -15,6 +17,10 @@ useHead({
     },
   ],
   script: [],
+});
+
+supabase.auth.onAuthStateChange((event, session) => {
+  console.log({ event, session });
 });
 </script>
 
